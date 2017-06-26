d369de3be02d3a97de97a13ecf05c6494060b850
rohanD <rohanoid5@gmail.com>
2017-06-26 09:26:12 +0530

    Added bundle.js.

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
+var express = require('express');
+var app = express();
 
-app.use(express.static(__dirname));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
+app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080, () => {
 	console.log("Application has been started!");
